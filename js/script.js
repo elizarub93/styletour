213906fb535c98575935e8ec4d3afe5bf051c341
Elizaveta <liru93@mail.ru>
2018-05-16 14:50:42 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function() {
+	$(".search").on('click',function(){
+		$('.main-menu .navbar-form').slideToggle();
+	});
+	$('#elastislide').elastislide();
+
+});
+$(window).load(function(){
+	var carouselCaptionWidth=$('#carousel-sidebar .active img').width();
+	$('#carousel-sidebar img').each(function(){
+		$(this).attr('width', carouselCaptionWidth);
+	});
+	$('#carousel-sidebar .sidebar-carousel-caption, .banner').css('width',carouselCaptionWidth+'px');
+	$('#carousel-sidebar .carousel-indicators').css('width',carouselCaptionWidth+'px');
+
+	
+});
